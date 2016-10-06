670a4568ba3f40a89b0f5530ec5a0775bca14ca3
lim <kudzuvalley@limui-MacBook-Air.local>
2016-10-06 23:17:40 +0900

    darwin 관련 사항을 추가했다.

--- core/emacs-setting.el
+++ core/emacs-setting.el
@@ -85,6 +85,28 @@
   ;; you may want to add different for other charset in this way.
   )
 
+(when (eq system-type 'darwin)
+  ;; default Latin font (e.g. Consolas)
+  ;; but I use Monaco
+  (message "MAC System")
+  (set-face-attribute 'default nil :family "Monaco")
+
+  ;; default font size (point * 10)
+  ;;
+  ;; WARNING!  Depending on the default font,
+  ;; if the size is not supported very well, the frame will be clipped
+  ;; so that the beginning of the buffer may not be visible correctly. 
+  (set-face-attribute 'default nil :height 130)
+
+  ;; use specific font for Korean charset.
+  ;; if you want to use different font size for specific charset,
+  ;; add :size POINT-SIZE in the font-spec.
+  (set-fontset-font t 'hangul (font-spec :name "NanumGothicCoding"))
+
+  ;; you may want to add different for other charset in this way.
+  )
+
+
 
 ;; ================================================================================
 ;; 선택된 영역을 C-d 를 이용해서 지우기 위한 설정이다.
@@ -191,6 +213,12 @@
 (setq scroll-margin 0
       scroll-conservatively 100000
       scroll-preserve-screen-position 1)
+
+
+(when (eq system-type 'darwin)
+  (setq mac-command-modifier 'meta)
+  (setq mac-option-modifier nil))
+
 
 
 
